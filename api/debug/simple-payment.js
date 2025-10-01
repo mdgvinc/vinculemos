96176f8139a78287d44792e3b202a109f4be5b96
mdgvinc <mdgvinc@outlook.com>
2025-09-30 23:01:10 -0300

    Update simple-payment.js

--- api/debug/simple-payment.js
+++ api/debug/simple-payment.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
@@ -36,4 +36,4 @@ module.exports = async (req, res) => {
       message: error.message 
     });
   }
-};
+}
