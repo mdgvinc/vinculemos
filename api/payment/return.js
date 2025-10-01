12b0e5b0cda983ca39ae8a1a2ffca92201543da3
mdgvinc <mdgvinc@outlook.com>
2025-10-01 00:23:03 -0300

    Delete api/payment directory

--- api/payment/return.js
+++
@@ -1,8 +0,0 @@
-// /api/payment/return.js
-export default function handler(req, res) {
-  // Flow sends ?token=xxxx in query
-  const { token } = req.query;
-
-  // Redirect to your success page
-  res.redirect(`/payment/success.html?token=${token}`);
-}
